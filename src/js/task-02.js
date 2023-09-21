63fc49e09ef88747bcdd376bf7716046342f303e
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2023-09-21 12:17:50 +0200

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const container = document.querySelector('#ingredients');
+const marcup = ingredients.map(
+  (e) => {
+    const liItem = document.createElement('li');
+    liItem.textContent = e;
+    liItem.className = 'item';
+    return liItem;
+  }
+)
+ container.append(...marcup);
+console.dir(container.outerHTML);
