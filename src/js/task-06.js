21bcf1c31a83b0d762350dbcb704f5a6032b2bfe
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2023-09-22 23:40:57 +0200

    task7

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,9 +1,8 @@
 const input = document.querySelector('#validation-input');
-let inputLength = input.getAttribute('data-length');
-inputLength = Number(inputLength);
+const inputLength = Number(input.dataset.length);
 const clbck = () => {
-  console.log(inputLength, typeof inputLength);
-  console.log(input.value.length, typeof input.value.length);
+  // console.log(inputLength, typeof inputLength);
+  // console.log(input.value.length, typeof input.value.length);
   if (input.value.length === inputLength) {
     input.classList.add('valid');
     input.classList.remove('invalid');
--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,11 +1,8 @@
-// ## Завдання 7
+const inputSize = document.querySelector('#font-size-control');
+const spanText = document.querySelector('#text');
 
-// // Напиши скрипт, який реагує на зміну значення `input#font-size-control` (подія `input`) 
-// і змінює інлайн - стиль`span#text`, оновлюючи властивість`font-size`.В результаті,
-//     перетягуючи повзунок, буде змінюватися розмір тексту.
+const clbck = () => {
+    spanText.style.fontSize = inputSize.value+'px';
+}
 
-// ```html
-// <input id="font-size-control" type="range" min="16" max="96" />
-// <br />
-// <span id="text">Abracadabra!</span>
-// ```
+inputSize.addEventListener('input', clbck);
